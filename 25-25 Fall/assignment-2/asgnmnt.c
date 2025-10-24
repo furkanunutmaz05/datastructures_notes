fc4086ccf3ab3a32df94da28bf718208d8721039
Batuhan Karadağ <52932122+batukar@users.noreply.github.com>
2025-10-24 15:58:04 +0300

    edited

---
+++ 25-25 Fall/assignment-2/asgnmnt.c
@@ -0,0 +1,12 @@
+// Ad:
+// Soyad:
+// Okul No:
+// Sınıf:
+// Öğrenim Türü (Örgün/İkinci Öğretim):
+
+#include <stdio.h>
+
+int main(){
+    printf("Merak etme, ödevler gayet basit.\nKendine güven ve bunu tamamla!\n");
+    return 0;
+}
