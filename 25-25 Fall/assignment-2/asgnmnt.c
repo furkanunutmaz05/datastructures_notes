af95935d55a41213202cd7eba56ae0504fd06a68
Furkan Unutmaz <156699603+furkanunutmaz05@users.noreply.github.com>
2025-10-29 22:05:55 +0300

    Update asgnmnt.c

--- 25-25 Fall/assignment-2/asgnmnt.c
+++ 25-25 Fall/assignment-2/asgnmnt.c
@@ -1,12 +1,6 @@
-// Ad:
-// Soyad:
-// Okul No:
-// Sınıf:
-// Öğrenim Türü (Örgün/İkinci Öğretim):
+// Ad:Furkan
+// Soyad:Unutmaz
+// Okul No:232523045
+// Sınıf:3.sınıf
+// Öğrenim Türü (Örgün/İkinci Öğretim):İkinci Öğretim
 
-#include <stdio.h>
-
-int main(){
-    printf("Merak etme, ödevler gayet basit.\nKendine güven ve bunu tamamla!\n");
-    return 0;
-}
